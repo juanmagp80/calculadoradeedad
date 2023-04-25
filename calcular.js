1fec4ab42cf83f2817b7c57e47d3289b5178a137
juanmagp80 <113768080+juanmagp80@users.noreply.github.com>
2023-04-25 22:16:07 +0200

    Add files via upload

---
+++ calcular.js
@@ -0,0 +1,27 @@
+
+		function calcularEdad() {
+			// Obtener valores de los campos de entrada
+			var dia = document.getElementById("dia").value;
+			var mes = document.getElementById("mes").value;
+			var anio = document.getElementById("anio").value;
+            // Verificar si los campos contienen valores válidos
+  if (dia === "" || mes === "" || anio === "") {
+    alert("Por favor, ingrese una fecha de nacimiento válida.");
+    return;
+  }
+
+
+			// Calcular la edad en milisegundos
+			var fechaNacimiento = new Date(anio, mes - 1, dia);
+			var edadEnMs = Date.now() - fechaNacimiento.getTime();
+
+			// Convertir la edad de milisegundos a días, meses y años
+			var edadEnDias = Math.floor(edadEnMs / 86400000);
+			var edadEnMeses = Math.floor(edadEnDias / 30.4368);
+			var edadEnAnios = Math.floor(edadEnMeses / 12);
+
+			// Mostrar resultado
+			document.getElementById("anios").innerHTML = "" + edadEnAnios;
+			document.getElementById("meses").innerHTML = "" + (edadEnMeses % 12);
+			document.getElementById("dias").innerHTML = "" + (edadEnDias % 30);
+		}
--- colorborde.js
+++ colorborde.js
@@ -2,11 +2,11 @@
 const inputs = document.querySelectorAll('myinput');
 
 inputs.forEach((input) => {
-  input.addEventListener('focus', (event) => {
+  myinput.addEventListener('focus', (event) => {
     event.target.style.borderColor = 'hsl(259, 100%, 65%)';
   });
 
-  input.addEventListener('blur', (event) => {
+  myinput.addEventListener('blur', (event) => {
     event.target.style.borderColor = '#ccc';
   });
 });
